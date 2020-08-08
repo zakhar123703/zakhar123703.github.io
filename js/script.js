3271ab7162b1c3db97ad959cbd2138ed47e3fe01
zakhar123703 <64077502+zakhar123703@users.noreply.github.com>
2020-08-08 19:30:32 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function changeColor(newColor) {
+  var elem = document.getElementById('c');
+  var elem1 = document.getElementById('b');
+  var elem2 = document.getElementById('a');
+  elem.style.background = newColor;
+   elem1.style.background = newColor;
+    elem2.style.background = newColor;  
+}
+
+function changeCod(newCod) {
+    var elem3 = document.getElementById('d');
+      var elem4 = document.getElementById('e');
+        var elem5 = document.getElementById('f');
+    elem3.style.background = newCod;
+    elem4.style.background = newCod;
+    elem5.style.background = newCod;
+}
